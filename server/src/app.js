205fc0bc7526b7e3c61961648e2e91b690ff0d84
Brendan McKeague <bmckeagu@Brendans-MacBook-Air.local>
2017-11-09 17:13:50 -0500

    update

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,12 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
-cont app = express()
+const app = express()
 
 app.use(morgan('combine'))
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 app.use(cors())
+
+app.listen(proccess.env.PORT || 8081)
