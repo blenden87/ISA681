4a4af71109c33cd78107027a5add14a85b74362d
Brendan McKeague <bmckeagu@Brendans-MacBook-Air.local>
2017-11-09 20:22:14 -0500

    Registration Page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: HelloWorld
+      component: Hello
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
